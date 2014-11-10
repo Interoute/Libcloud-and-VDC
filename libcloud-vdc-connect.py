78e724ade89c2d5abde1690ba062a9df1713a8ed
Costas <costas@cpapanicolaou.com>
2014-11-10 15:02:39 +0000

    Tidying up
    Connection.py allows a user to connect to VDC 2.0 using libcloud

---
+++ libcloud-vdc-connect.py
@@ -0,0 +1,14 @@
+#import of libcloud libraries
+from libcloud.compute.types import Provider
+from libcloud.compute.providers import get_driver
+
+#create a driver for VDC
+VDCDriver=get_driver(Provider.CLOUDSTACK)
+
+#set VDC connection details
+vdc_apikey="INSERT YOUR VDC'S ACCOUNT API KEY HERE"
+vdc_secretkey="INSERT YOUR VDC'S ACCOUNT SECRET KEY HERE"
+vdc_url="https://myservices.interoute.com/myservices/api/vdc"
+
+#create connection to VDC
+vdc_conn=VDCDriver(key=vdc_apikey, secret=vdc_secretkey, url=vdc_url)
