49ba9ad73195923a2853a9fc4b342cde1d25d732
costas88 <COSTAS@CPapanicolaou.com>
2014-11-27 13:58:42 +0000

    Update create_node.py

--- create_node.py
+++ create_node.py
@@ -1,5 +1,8 @@
 #Import of the connection driver
 import connection as vdc
+from libcloud.compute.base import NodeAuthPassword
+
+
 
 #Task: Creating a VM on Interoute's VDC 2.0
 #Setting the configuration details for the VM to be created
@@ -7,16 +10,18 @@ image = [image for image in vdc.getConn().list_images() if 'IRT-CENTOS-6.5' in i
 size = [size for size in vdc.getConn().list_sizes() if size.name == '6144-2'][0]
 net = [network for network in vdc.getConn().ex_list_networks() if 'London' in network.name]
 location = [location for location in vdc.getConn().list_locations() if 'London' in location.name][0]
-name = 'Node1'
+name = 'Interoute-Node'
 
+#Creating the VM - Equivalent libcloud command: create_node
+print 'Creating node: ' + name
+if len(net) == 0 :
+  node = [vdc.getConn().create_node(name=name, image=image, size=size, location=location)]
+else:
+  node = [vdc.getConn().create_node(name=name, image=image, size=size, location=location, networks=[net[0]])]
+print 'Created node: ' + name
+print 'Password: ' + node[0].extra['password']
 
-#If the VM with name'Node1' doesn't exist then create it.
-node = [node for node in vdc.getConn().list_nodes() if name == node.name]
-if node==[]:
-  
-  #Creating the VM - Equivalent libcloud command: create_node
-  print 'Creating node: ' + name
-  if size(net) == 0 :
-    node = [vdc.getConn().create_node(name=name, image=image, size=size, location=location)]
-  else:
-    node = [vdc.getConn().create_node(name=name, image=image, size=size, location=location, networks=[net[0]])]
+#Method getNode
+#Returns the node
+def getNode():
+        return node
