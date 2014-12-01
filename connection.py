bd1a54f55abcccf3d7b0cd0160811d2e350e922c
Phillip Kent <phillip.kent@gmail.com>
2014-12-01 17:45:48 +0000

    update connector.py

--- connection.py
+++ connection.py
@@ -6,9 +6,9 @@ from libcloud.compute.providers import get_driver
 VDCDriver=get_driver(Provider.CLOUDSTACK)
 
 #set VDC connection details
-vdc_apikey= '61C08D-OJFE4bur88saOa9z_gZSFOCixiaVcqGzSkQcdTPnHyRePyibeR2KeADvH1Jo6T8aUhVwTMVT5KCuArA'
-vdc_secretkey= 'ObAgu3Wp3xXs1n8YXGntc2nUAhsBuuVQHRmHNECTn_f9anITv75lK_QRDt2XwrTruFaflcWpOSXH2A2khNtnQg'
-vdc_url= 'https://myservices.interoute.com/myservices/api/vdc'
+vdc_apikey= 'INSERT YOUR VDC ACCOUNT API KEY HERE'
+vdc_secretkey= 'INSERT YOUR VDC ACCOUNT SECRET KEY HERE'
+vdc_url= 'INSERT YOUR VDC ACCESS URL HERE'
 
 #create connection to VDC
 conn=VDCDriver(key=vdc_apikey, secret=vdc_secretkey, url=vdc_url)
